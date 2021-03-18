a25bb6c6b761283eeb1d118a719ad8b3db0d9e50
Davi10001 <77360335+Davi10001@users.noreply.github.com>
2021-03-17 23:41:24 -0300

    Add files via upload

--- src/daftarvip.js
+++ src/daftarvip.js
@@ -1,4 +1,4 @@
 const daftarvip = (prefix) => { 
-	return `PARA SE TORNAR VIP/PREMIUN ENTRE EM CONTATO COM O CRIADOR SÓ DIGITE ${prefix}owner`
+	return `PARA SE TORNAR VIP/PREMIUN ENTRE EM CONTATO COM O CRIADOR SÓ VER O CTT DELE NO MENU DIGITE ${prefix}help ou ${prefix}menu`
 }
 exports.daftarvip = daftarvip
