612f54361fc01a9e2e16f6b8a04306d9c4d26182
Davi10001 <77360335+Davi10001@users.noreply.github.com>
2021-04-02 01:38:40 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -300,11 +300,11 @@ async function starts() {
 			}
 
 			const botNumber = client.user.jid
-			const ownerNumber = ["5522996215481@s.whatsapp.net"] // replace this with your number
-			const mod = [ownerNumber,"5522996215481@s.whatsapp.net"]//ubah nomor lo
-			const adminbotnumber = ["5522996215481@s.whatsapp.net"]//ubah nomor lo
-			const frendsowner = ["5522996215481@s.whatsapp.net"]//ubah nomor lo
-			const premium = ["5522996215481@s.whatsapp.net","559784130980@s.whatsapp.net","557599941062@s.whatsapp.net","558598669655@s.whatsapp.net","5511949051934@s.whatsapp.net","554792091566@s.whatsapp.net","558699541889@s.whatsapp.net","559294313229@s.whatsapp.net","554298653614@s.whatsapp.net"]
+			const ownerNumber = ["553188514445@s.whatsapp.net"] // replace this with your number
+			const mod = [ownerNumber,"553188514445@s.whatsapp.net"]//ubah nomor lo
+			const adminbotnumber = ["553188514445@s.whatsapp.net"]//ubah nomor lo
+			const frendsowner = ["553188514445@s.whatsapp.net"]//ubah nomor lo
+			const premium = ["553188514445@s.whatsapp.net"]
 			const isGroup = from.endsWith('@g.us')
 			const sender = isGroup ? mek.participant : mek.key.remoteJid
 			const groupMetadata = isGroup ? await client.groupMetadata(from) : ''
